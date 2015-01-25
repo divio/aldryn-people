1031548a52e3135df66051b9dd4dd9bb454ad05b
Martin Koistinen <mkoistinen@gmail.com>
2015-01-25 03:32:24 +0200

    Add phoney haystack configuration

--- test_settings.py
+++ test_settings.py
@@ -1,5 +1,31 @@
+HAYSTACK_CONNECTIONS = {
+    'default': {
+        'ENGINE': 'haystack.backends.solr_backend.SolrEngine',
+        'URL': 'http://localhost:9001/solr/default',
+        'TIMEOUT': 60 * 5,
+        'INCLUDE_SPELLING': True,
+        'BATCH_SIZE': 100,
+        'EXCLUDED_INDEXES': ['thirdpartyapp.search_indexes.BarIndex'],
+    },
+    'en': {
+        'ENGINE': 'haystack.backends.solr_backend.SolrEngine',
+        'URL': 'http://my-solr-server/solr/my-site-en/',
+        'TIMEOUT': 60 * 5,
+        'INCLUDE_SPELLING': True,
+        'BATCH_SIZE': 100,
+    },
+    'de': {
+        'ENGINE': 'haystack.backends.solr_backend.SolrEngine',
+        'URL': 'http://my-solr-server/solr/my-site-de/',
+        'TIMEOUT': 60 * 5,
+        'INCLUDE_SPELLING': True,
+        'BATCH_SIZE': 100,
+    },
+}
+
 HELPER_SETTINGS = {
     'TIME_ZONE': 'Europe/Zurich',
+    'HAYSTACK_CONNECTIONS': HAYSTACK_CONNECTIONS,
     'INSTALLED_APPS': [
         'filer',
         'easy_thumbnails',
