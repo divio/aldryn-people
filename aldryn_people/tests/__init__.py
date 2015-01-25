b87ff3b5009bb991cca16cd75a9dbbcb88ae5344
Martin Koistinen <mkoistinen@gmail.com>
2015-01-25 03:33:58 +0200

    Substantially improve tests for models

--- aldryn_people/tests/__init__.py
+++ aldryn_people/tests/__init__.py
@@ -4,16 +4,75 @@ from __future__ import unicode_literals
 
 from django.conf import settings
 from django.contrib.auth.models import User
-from django.test import TestCase
+from django.contrib.sites.models import Site
+from django.db import IntegrityError
+from django.test import RequestFactory, TestCase
+from django.utils.translation import override
 
 from cms import api
-from cms.utils import get_cms_setting
+from cms.models import Title
 from cms.test_utils.testcases import BaseCMSTestCase
+from cms.utils import get_cms_setting
+from cms.utils.i18n import get_language_list
+
+from djangocms_helper.utils import create_user
+
+from ..models import Group, Person
 
 
 class BasePeopleTest(TestCase, BaseCMSTestCase):
     su_username = 'user'
     su_password = 'pass'
+
+    data = {
+        'group1': {
+            'en': {'name': 'group1', 'description': 'description1'},
+            'de': {'name': 'Gruppe1', 'description': 'Beschreibung1'},
+        },
+        'group2': {
+            # This should *not* have a EN translation
+            'de': {'name': 'Gruppe2', 'description': 'Beschreibung2'},
+        },
+        'person1': {
+            'en': {'function': 'function1', 'description': 'description-en'},
+            'de': {'function': 'Funktion1', 'description': 'Beschreibung-de'},
+        },
+        'person2': {
+            # This should *not* have a EN translation
+            'de': {'function': 'Funktion2', 'description': 'Beschreibung2'},
+        },
+    }
+
+    @staticmethod
+    def reload(obj, language=None):
+        """Simple convenience method for re-fetching an object from the ORM,
+        optionally "as" a specified language."""
+        try:
+            new_obj = obj.__class__.objects.language(language).get(id=obj.id)
+        except:
+            new_obj = obj.__class__.objects.get(id=obj.id)
+        return new_obj
+
+    def assertEqualItems(self, a, b):
+        try:
+            # In Python3, this method has been renamed (poorly)
+            return self.assertCountEqual(a, b)
+        except:
+            # In 2.6, assertItemsEqual() doesn't sort first
+            return self.assertItemsEqual(sorted(a), sorted(b))
+
+    def mktranslation(self, obj, lang, **kwargs):
+        """Simple method of adding a translation to an existing object."""
+        try:
+            obj.set_current_language(lang)
+        except:
+            try:
+                obj.translate(lang)
+            except IntegrityError:
+                pass
+        for k, v in kwargs.items():
+            setattr(obj, k, v)
+        obj.save()
 
     def setUp(self):
         self.template = get_cms_setting('TEMPLATES')[0][0]
@@ -22,7 +81,88 @@ class BasePeopleTest(TestCase, BaseCMSTestCase):
             'page', self.template, self.language, published=True)
         self.placeholder = self.page.placeholders.all()[0]
         self.superuser = self.create_superuser()
+        with override('en'):
+            self.person1 = Person(**self.data['person1']['en'])
+            self.group1 = Group(**self.data['group1']['en'])
+        self.person1.name = 'person1'
+        self.person1.slug = 'person1-slug'
+        self.person1.save()
+        self.group1.save()
+
+        # Add a DE translation for person1, group1
+        self.mktranslation(self.person1, 'de', **self.data['person1']['de'])
+        self.mktranslation(self.group1, 'de', **self.data['group1']['de'])
+
+        # Make person2, group2
+        with override('de'):
+            self.person2 = Person(**self.data['person2']['de'])
+            self.group2 = Group(**self.data['group2']['de'])
+        self.person2.name = 'person2'
+        self.person2.slug = 'person2-slug'
+        self.person2.save()
+        self.group2.save()
 
     def create_superuser(self):
         return User.objects.create_superuser(
             self.su_username, 'email@example.com', self.su_password)
+
+
+class CMSRequestBasedTest(TestCase):
+    """Sets-up User(s) and CMS Pages for testing."""
+    languages = get_language_list()
+
+    @classmethod
+    def setUpClass(cls):
+        cls.request_factory = RequestFactory()
+        cls.user = create_user('normal', 'normal@admin.com', 'normal')
+        cls.site1 = Site.objects.get(pk=1)
+
+    @classmethod
+    def tearDownClass(cls):
+        User.objects.all().delete()
+
+    def get_or_create_page(self, base_title=None, languages=None):
+        """Creates a page with a given title, or, if it already exists, just
+        retrieves and returns it."""
+        from cms.api import create_page, create_title
+        if not base_title:
+            # No title? Create one.
+            base_title = self.rand_str(prefix="page", length=8)
+        if not languages:
+            # If no langs supplied, use'em all
+            languages = self.languages
+        # If there is already a page with this title, just return it.
+        try:
+            page_title = Title.objects.get(title=base_title)
+            return page_title.page.get_draft_object()
+        except:
+            pass
+
+        # No? Okay, create one.
+        page = create_page(base_title, 'fullwidth.html', language=languages[0])
+        # If there are multiple languages, create the translations
+        if len(languages) > 1:
+            for lang in languages[1:]:
+                title_lang = "{0}-{1}".format(base_title, lang)
+                create_title(language=lang, title=title_lang, page=page)
+                page.publish(lang)
+        return page.get_draft_object()
+
+    def get_page_request(
+            self, page, user, path=None, edit=False, lang_code='en'):
+        from cms.middleware.toolbar import ToolbarMiddleware
+        path = path or page and page.get_absolute_url()
+        if edit:
+            path += '?edit'
+        request = RequestFactory().get(path)
+        request.session = {}
+        request.user = user
+        request.LANGUAGE_CODE = lang_code
+        if edit:
+            request.GET = {'edit': None}
+        else:
+            request.GET = {'edit_off': None}
+        request.current_page = page
+        mid = ToolbarMiddleware()
+        mid.process_request(request)
+        return request
--- aldryn_people/tests/test_models.py
+++ aldryn_people/tests/test_models.py
@@ -2,60 +2,77 @@
 
 from __future__ import unicode_literals
 
+from django.core.urlresolvers import reverse
+from django.test import TransactionTestCase
+from django.utils.translation import override, force_text
+
 from ..models import Person, Group
 
 from . import BasePeopleTest
 
 
-class TestPeopleModels(BasePeopleTest):
+class TestBasicPeopleModels(TransactionTestCase):
 
     def test_create_person(self):
-        """
-        We can create a person with a name
-        """
+        """We can create a person with a name."""
         name = 'Tom Test'
         person = Person.objects.create(name=name)
         self.assertEqual(person.name, name)
         self.assertEqual(Person.objects.all()[0], person)
 
     def test_delete_person(self):
-        """
-        We can delete a person
-        """
+        """We can delete a person."""
         name = 'Person Delete'
         Person.objects.create(name=name)
         Person.objects.get(name=name).delete()
         self.assertFalse(Person.objects.filter(name=name))
 
+    def test_str(self):
+        name = 'Person Str'
+        person = Person.objects.create(name=name)
+        self.assertEqual(force_text(person), name)
+
+    def test_absolute_url(self):
+        slug = 'person-slug'
+        person = Person.objects.create(slug=slug)
+        # This isn't a translation test, per se, but let's make sure that we
+        # have a predictable language prefix, regardless of the tester's locale.
+        with override('en'):
+            self.assertEqual(
+                person.get_absolute_url(),
+                '/en/{0}/'.format(slug)
+            )
+            # Now test that it will work when there's no slug too.
+            person.slug = ''
+            self.assertEqual(
+                person.get_absolute_url(),
+                '/en/{0}/'.format(person.pk),
+            )
+
+
+class TestBasicGroupModel(TransactionTestCase):
+
     def test_create_group(self):
-        """
-        We can create a group with a name
-        """
+        """We can create a group with a name."""
         group = Group.objects.create(name='group_b')
         self.assertTrue(group.name, 'group_b')
 
     def test_delete_group(self):
-        """
-        We can delete a group
-        """
+        """We can delete a group."""
         name = 'Gruop Delete'
         Group.objects.create(name=name)
         Group.objects.using_translations().get(name=name).delete()
         self.assertFalse(Group.objects.using_translations().filter(name=name))
 
     def test_create_another_group(self):
-        """
-        we create a group
-        """
+        """we create a group."""
         name = 'Gruppe Neu'
         group = Group.objects.create(name=name)
         self.assertEqual(group.name, name)
         self.assertEqual(Group.objects.all()[0], group)
 
     def test_add_person_to_group(self):
-        """
-        We create a person and add her to the created group
-        """
+        """We create a person and add her to the created group."""
         personname = 'Daniel'
         person = Person.objects.create(name=personname)
         name = 'Group One'
@@ -63,3 +80,113 @@ class TestPeopleModels(BasePeopleTest):
         person.group = group
         person.save()
         self.assertIn(person, group.person_set.all())
+
+
+class TestPersonModelTranslation(BasePeopleTest):
+
+    def test_person_translatable(self):
+        person1 = self.reload(self.person1, 'en')
+        self.assertEqual(
+            person1.function,
+            self.data['person1']['en']['function']
+        )
+        person1 = self.reload(self.person1, 'de')
+        self.assertEqual(
+            person1.lazy_translation_getter('function'),
+            self.data['person1']['de']['function']
+        )
+
+    def test_comment(self):
+        person1 = self.reload(self.person1, 'en')
+        self.assertEqual(
+            person1.comment,
+            self.data['person1']['en']['description']
+        )
+        person1 = self.reload(self.person1, 'de')
+        self.assertEqual(
+            person1.comment,
+            self.data['person1']['de']['description']
+        )
+
+    def test_get_vcard(self):
+        person1 = self.reload(self.person1, 'en')
+        # Test with no group
+        vcard_en = 'BEGIN:VCARD\r\nVERSION:3.0\r\nFN:person1\r\nN:;person1;;;\r\nTITLE:function1\r\nEND:VCARD\r\n'  # flake8: noqa
+        self.assertEqual(
+            person1.get_vcard(),
+            vcard_en
+        )
+        # Test with a group and other fields populated
+        group1 = self.reload(self.group1, 'en')
+        group1.address = '123 Main Street'
+        group1.city = 'Anytown'
+        group1.postal_code = '12345'
+        group1.phone = '+1 (234) 567-8903'
+        group1.fax = '+1 (234) 567-8904'
+        group1.website = 'www.groupwebsite.com'
+        group1.save()
+        person1.group = group1
+        person1.email = 'person@org.org'
+        person1.phone = '+1 (234) 567-8900'
+        person1.mobile = '+1 (234) 567-8901'
+        person1.fax = '+1 (234) 567-8902'
+        person1.website = 'www.website.com'
+        person1.save()
+        vcard_en = 'BEGIN:VCARD\r\nVERSION:3.0\r\nADR;TYPE=WORK:;;123 Main Street;Anytown;;12345;\r\nEMAIL:person@org.org\r\nFN:person1\r\nN:;person1;;;\r\nORG:group1\r\nTEL;TYPE=WORK:+1 (234) 567-8900\r\nTEL;TYPE=CELL:+1 (234) 567-8901\r\nTEL;TYPE=FAX:+1 (234) 567-8902\r\nTEL;TYPE=WORK:+1 (234) 567-8903\r\nTEL;TYPE=FAX:+1 (234) 567-8904\r\nTITLE:function1\r\nURL:www.website.com\r\nURL:www.groupwebsite.com\r\nEND:VCARD\r\n'  # flake8: noqa
+        self.assertEqual(
+            person1.get_vcard(),
+            vcard_en
+        )
+        # Ensure this works for other langs too
+        person1 = self.reload(self.person1, 'de')
+        vcard_de = 'BEGIN:VCARD\r\nVERSION:3.0\r\nADR;TYPE=WORK:;;123 Main Street;Anytown;;12345;\r\nEMAIL:person@org.org\r\nFN:person1\r\nN:;person1;;;\r\nORG:Gruppe1\r\nTEL;TYPE=WORK:+1 (234) 567-8900\r\nTEL;TYPE=CELL:+1 (234) 567-8901\r\nTEL;TYPE=FAX:+1 (234) 567-8902\r\nTEL;TYPE=WORK:+1 (234) 567-8903\r\nTEL;TYPE=FAX:+1 (234) 567-8904\r\nTITLE:Funktion1\r\nURL:www.website.com\r\nURL:www.groupwebsite.com\r\nEND:VCARD\r\n'  # flake8: noqa
+        with override('de'):
+            self.assertEqual(
+                person1.get_vcard(),
+                vcard_de
+            )
+
+
+class TestGroupModelTranslation(BasePeopleTest):
+
+    def test_group_translation(self):
+        group1 = self.reload(self.group1, 'en')
+        self.assertEqual(group1.name, self.data['group1']['en']['name'])
+        group1 = self.reload(self.group1, 'de')
+        self.assertEqual(group1.name, self.data['group1']['de']['name'])
+
+    def test_company_name(self):
+        group1 = self.reload(self.group1, 'en')
+        self.assertEqual(
+            group1.company_name,
+            self.data['group1']['en']['name'],
+        )
+        group1 = self.reload(self.group1, 'de')
+        self.assertEqual(
+            group1.company_name,
+            self.data['group1']['de']['name'],
+        )
+
+    def test_company_description(self):
+        group1 = self.reload(self.group1, 'en')
+        self.assertEqual(
+            group1.company_description,
+            self.data['group1']['en']['description'],
+        )
+        group1 = self.reload(self.group1, 'de')
+        self.assertEqual(
+            group1.company_description,
+            self.data['group1']['de']['description'],
+        )
+
+    def test_str(self):
+        group1 = self.reload(self.group1, 'en')
+        self.assertEqual(
+            force_text(group1),
+            self.data['group1']['en']['name'],
+        )
+        group1 = self.reload(self.group1, 'de')
+        self.assertEqual(
+            force_text(group1),
+            self.data['group1']['de']['name'],
+        )
