40c99dd04429cd267dc104508b988672a2feec54
Angelo Dini <angelo.dini@divio.ch>
2015-07-15 11:51:05 +0200

    basic cleanup

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -1,17 +1,20 @@
+CHANGELOG
+=========
+
 0.5.3 (2015-07-08)
-==================
+------------------
 
 * Added another missing django >= 1.7 migration
 * Fixed integrity error on automatic slug generation
 
 0.5.2 (2015-04-25)
-==================
+------------------
 
 * Added missing django >= 1.7 migration
 * Dropped support for django 1.4 & 1.5
 * Updated requirements to require aldryn-common>=0.1.3
 
 0.5.1 (2015-04-16)
-==================
+------------------
 
 * Use get_current_language from cms instead get_language from Django because Django bug #9340
--- setup.py
+++ setup.py
@@ -3,24 +3,29 @@ from setuptools import setup, find_packages
 from aldryn_people import __version__
 
 REQUIREMENTS = [
-    'django-parler',
-    'phonenumbers',
-    'django-phonenumber-field>=0.7.2',  # only here as quick-fix because old migrations depend on this field
-    'vobject',
-    'django-filer',
-    'aldryn-common>=0.1.3',
-    'djangocms-text-ckeditor',
+    'Django>=1.6,<1.8',
     'aldryn-boilerplates',
+    'aldryn-common>=0.1.3',
+    'django-parler',
+    'django-phonenumber-field>=0.7.2',  # only here as quick-fix because old migrations depend on this field
+    'django-filer',
+    'djangocms-text-ckeditor',
+    'phonenumbers',
+    'vobject',
 ]
 
 CLASSIFIERS = [
-    'Development Status :: 2 - Pre-Alpha',
+    'Development Status :: 4 - Beta',
     'Environment :: Web Environment',
     'Framework :: Django',
+    'Framework :: Django :: 1.6',
+    'Framework :: Django :: 1.7',
     'Intended Audience :: Developers',
     'License :: OSI Approved :: BSD License',
     'Operating System :: OS Independent',
     'Programming Language :: Python',
+    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2.7',
     'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     'Topic :: Software Development',
     'Topic :: Software Development :: Libraries :: Application Frameworks',
@@ -38,7 +43,7 @@ setup(
     platforms=['OS Independent'],
     install_requires=REQUIREMENTS,
     classifiers=CLASSIFIERS,
+    long_description=open('README.rst').read(),
     include_package_data=True,
     zip_safe=False,
-    test_suite='cms_helper.run',
 )
