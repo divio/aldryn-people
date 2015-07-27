fd9208c446b404e7e80ee20b44bd153506ace2ac
Martin Koistinen <mkoistinen@gmail.com>
2015-07-27 12:48:41 -0400

    Adds group/people counts to admin list views

--- aldryn_people/admin.py
+++ aldryn_people/admin.py
@@ -3,6 +3,7 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from django.db.models import Count
 from django.utils.translation import ugettext_lazy as _
 
 from parler.admin import TranslatableAdmin
@@ -13,7 +14,7 @@ from .models import Person, Group
 
 class PersonAdmin(AllTranslationsMixin, TranslatableAdmin):
     list_display = [
-        '__str__', 'email', 'vcard_enabled', ]
+        '__str__', 'email', 'vcard_enabled', 'num_groups', ]
     list_filter = ['groups', 'vcard_enabled']
     search_fields = ('translations__name', 'email', 'translations__function')
     raw_id_fields = ('user',)
@@ -38,10 +39,20 @@ class PersonAdmin(AllTranslationsMixin, TranslatableAdmin):
         }),
     )
 
+    def get_queryset(self, request):
+        qs = super(PersonAdmin, self).queryset(request)
+        qs = qs.annotate(group_count=Count('groups'))
+        return qs
+
+    def num_groups(self, obj):
+        return obj.group_count
+    num_groups.short_description = '# Groups'
+    num_groups.admin_order_field = 'group_count'
+
 
 class GroupAdmin(AllTranslationsMixin, TranslatableAdmin):
 
-    list_display = ['__str__', 'city', ]
+    list_display = ['__str__', 'city', 'num_people', ]
     search_filter = ['translations__name']
     fieldsets = (
         (None, {
@@ -57,6 +68,16 @@ class GroupAdmin(AllTranslationsMixin, TranslatableAdmin):
             )
         }),
     )
+
+    def get_queryset(self, request):
+        qs = super(GroupAdmin, self).queryset(request)
+        qs = qs.annotate(people_count=Count('people'))
+        return qs
+
+    def num_people(self, obj):
+        return obj.people_count
+    num_people.short_description = '# People'
+    num_people.admin_order_field = 'people_count'
 
 
 admin.site.register(Person, PersonAdmin)
