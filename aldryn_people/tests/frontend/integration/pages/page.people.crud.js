48912c7ea890e45308e28ac9496a8e438c69b0fb
Marketionist <marketionist@gmail.com>
2015-07-30 17:06:54 +0300

    Added log in test

--- aldryn_people/tests/frontend/integration/pages/page.people.crud.js
+++ aldryn_people/tests/frontend/integration/pages/page.people.crud.js
@@ -4,6 +4,7 @@
  */
 
 'use strict';
+/* global element, by, browser, expect */
 
 // #############################################################################
 // INTEGRATION TEST PAGE OBJECT
@@ -11,7 +12,45 @@
 var peoplePage = {
     site: 'http://127.0.0.1:8000/en/',
     mainElementsWaitTime: 12000,
-    iframeWaitTime: 15000
+    iframeWaitTime: 15000,
+
+    // log in
+    editModeLink: element(by.css('.inner a[href="/?edit"]')),
+    usernameInput: element(by.id('id_cms-username')),
+    passwordInput: element(by.id('id_cms-password')),
+    loginButton: element(by.css('.cms_form-login input[type="submit"]')),
+    userMenus: element.all(by.css('.cms_toolbar-item-navigation > li > a')),
+    testLink: element(by.css('.selected a')),
+
+    cmsLogin: function (credentials) {
+        // object can contain username and password, if not set it will
+        // fallback to 'admin'
+        credentials = credentials ||
+            { username: 'admin', password: 'admin' };
+
+        peoplePage.usernameInput.clear();
+
+        // fill in email field
+        peoplePage.usernameInput.sendKeys(
+            credentials.username).then(function () {
+            peoplePage.passwordInput.clear();
+
+            // fill in password field
+            return peoplePage.passwordInput.sendKeys(
+                credentials.password);
+        }).then(function () {
+            peoplePage.loginButton.click();
+
+            // wait for user menu to appear
+            browser.wait(browser.isElementPresent(
+                peoplePage.userMenus.first()),
+                peoplePage.mainElementsWaitTime);
+
+            // validate user menu
+            expect(peoplePage.userMenus.first().isDisplayed())
+                .toBeTruthy();
+        });
+    }
 
 };
 
--- aldryn_people/tests/frontend/integration/specs/spec.people.crud.js
+++ aldryn_people/tests/frontend/integration/specs/spec.people.crud.js
@@ -11,9 +11,28 @@
 var peoplePage = require('../pages/page.people.crud.js');
 
 describe('Aldryn People tests: ', function () {
-    it('opens the main page', function () {
+    it('logs in to the site with valid username and password', function () {
         // go to the main page
         browser.get(peoplePage.site);
+
+        // check if the page already exists
+        peoplePage.testLink.isPresent().then(function (present) {
+            if (present === true) {
+                // go to the main page
+                browser.get(peoplePage.site + '?edit');
+            } else {
+                // click edit mode link
+                peoplePage.editModeLink.click();
+            }
+
+            // wait for username input to appear
+            browser.wait(function () {
+                return browser.isElementPresent(peoplePage.usernameInput);
+            }, peoplePage.mainElementsWaitTime);
+
+            // login to the site
+            peoplePage.cmsLogin();
+        });
     });
 
 });
