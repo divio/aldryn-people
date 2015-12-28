bfce61453f5341b3ae1c6b70899f49deb838a39e
Andrea Stagi <stagi.andrea@gmail.com>
2015-12-28 16:28:49 +0100

    Use safe_translation_getter for name

--- aldryn_people/models.py
+++ aldryn_people/models.py
@@ -216,7 +216,7 @@ class Person(TranslationHelperMixin, TranslatedAutoSlugifyMixin,
 
         if six.PY2:
             pkstr = six.u(pkstr)
-        name = self.name.strip()
+        name = self.safe_translation_getter('name', default='', any_language=True).strip()
         return name if len(name) > 0 else pkstr
 
     @property
