501400086ee012a9504d9b10bae803c6fa479108
Martin Koistinen <mkoistinen@gmail.com>
2016-05-18 13:14:29 -0400

    Fix disabled test

--- aldryn_people/tests/test_admin.py
+++ aldryn_people/tests/test_admin.py
@@ -2,11 +2,11 @@
 
 from __future__ import unicode_literals
 
-from django.contrib.sites.models import Site
-from django.utils.translation import override
+from django.contrib import admin
+
+from cms.utils.urlutils import admin_reverse
 
 from ..admin import PersonAdmin
-# from ..models import Person
 
 from . import BasePeopleTest
 
@@ -14,21 +14,13 @@ from . import BasePeopleTest
 class TestPersonAdmin(BasePeopleTest):
 
     def test_all_translations(self):
-        site = Site.objects.get(pk=1)
-        model_admin = PersonAdmin(self.person1, site)
+        # Check that all the available languages appear in `all_translations`
+        model_admin = PersonAdmin(self.person1, admin.site)
         all_translations = model_admin.all_translations(self.person1)
         obj_id = self.person1.id
 
-        # This test needs to be fixed
-        # with override('en'):
-        #     self.assertEqual(
-        #         all_translations,
-        #         '<strong><a href="/en/admin/aldryn_people/person/{id}/?language=en">en</a></strong>, <a href="/en/admin/aldryn_people/person/{id}/?language=de">de</a>'.format(id=obj_id)  # flake8: noqa
-        #     )
+        change_url = admin_reverse('aldryn_people_person_change', args=[obj_id])
 
-        # This test does not pass
-        # with override('de'):
-        #     self.assertEqual(
-        #         all_translations,
-        #         '<a href="/en/admin/aldryn_people/person/{id}/?language=en">en</a>, <strong><a href="/en/admin/aldryn_people/person/{id}/?language=de">de</a></strong>'.format(id=obj_id)  # flake8: noqa
-        #     )
+        self.assertTrue(change_url + '?language=en' in all_translations)
+        self.assertTrue(change_url + '?language=de' in all_translations)
+        self.assertTrue(change_url + '?language=fr' in all_translations)
